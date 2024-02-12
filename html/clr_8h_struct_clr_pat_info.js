c876dc49b64a7380019d527d2bd03b0cfcb8feac
roghelm <92537117+roghelm@users.noreply.github.com>
2024-02-12 14:18:29 -0500

    Add files via upload

---
+++ html/clr_8h_struct_clr_pat_info.js
@@ -0,0 +1,13 @@
+var clr_8h_struct_clr_pat_info =
+[
+    [ "implementation", "clr_8h.html#a9a417a9bdc0ed272e599ae046b04b09c", null ],
+    [ "bbox", "clr_8h.html#ad2c663a87abb5bb61fdf90b766ce1cdf", null ],
+    [ "mtx", "clr_8h.html#a026c847fc83269596da7c28a8eaedb7d", null ],
+    [ "xstep", "clr_8h.html#a6a68df5a9da766a9b6bf8a044c430bc9", null ],
+    [ "ystep", "clr_8h.html#a79ec21f26b59306f7839b65ff196080b", null ],
+    [ "xOriginDev", "clr_8h.html#a01edfd4681fbabb37993c810cf9f4189", null ],
+    [ "yOriginDev", "clr_8h.html#a0ced3f881202d4d2f5ca5b68899b096b", null ],
+    [ "paintType", "clr_8h.html#affdec09da4acea1f4b422c622938e5d2", null ],
+    [ "tilingType", "clr_8h.html#aeb3fc751102bf3358bba9a61b7cdb619", null ],
+    [ "scaleMtx", "clr_8h.html#adc6134ba2e646d75ef5f33a1e31abb82", null ]
+];
---
+++ html/dir_1d74fb0e9e65619156cf806c1fb80787.js
@@ -0,0 +1,5 @@
+var dir_1d74fb0e9e65619156cf806c1fb80787 =
+[
+    [ "pdfrw.h", "pdfrw_8h.html", "pdfrw_8h" ],
+    [ "pdfupd.h", "pdfupd_8h.html", "pdfupd_8h" ]
+];
