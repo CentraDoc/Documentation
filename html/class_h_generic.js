a580c0d991a39da546821338669a5adcb6d95b52
roghelm <92537117+roghelm@users.noreply.github.com>
2024-02-12 13:50:08 -0500

    Add files via upload

---
+++ html/class_h_generic.js
@@ -0,0 +1,15 @@
+var class_h_generic =
+[
+    [ "HGeneric", "class_h_generic.html#a56a2929d7f4d1a42f869c251cf31b2f7", null ],
+    [ "HGeneric", "class_h_generic.html#a485e2a414960e7005cc4a3d95c5125c1", null ],
+    [ "~HGeneric", "class_h_generic.html#a5e3423c63250fcc76ca838b772b93265", null ],
+    [ "Use", "class_h_generic.html#aea75b45d2d1a866ccd54ff82dd80e486", null ],
+    [ "Discard", "class_h_generic.html#acff21064fee571ef4901beb96126a9c2", null ],
+    [ "operator=", "class_h_generic.html#a04f8aac8b08cf3418c289cf4f89517ab", null ],
+    [ "ptr", "class_h_generic.html#a6affd351b685c2ca612bd758f330c3e0", null ],
+    [ "reference", "class_h_generic.html#aad65c7d44d59d5800eaf4b1abe568259", null ],
+    [ "Clone", "class_h_generic.html#a23f9220fc9436f4d2e5eb57ea1f920c6", null ],
+    [ "operator int", "class_h_generic.html#abd5186e31809856831d7d8f82df7d2cb", null ],
+    [ "classOf", "class_h_generic.html#ae6be11cbd2bbcf8af479b678747290bf", null ],
+    [ "className", "class_h_generic.html#a992f4cd05a5536f8ef675210e1e4c206", null ]
+];
